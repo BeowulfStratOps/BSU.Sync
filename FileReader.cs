2c7716e30dc69c5bc839bb265c81f6ffd81de33c
James McCartney <Jsm123@gmail.com>
2016-02-15 01:17:57 +0000

    Added ReadServerFileFromStream method to FIleReader class

---
+++ FileReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BSO.Sync.FileTypes;
+using Newtonsoft.Json;
+
+namespace BSO.Sync
+{
+    internal static class FileReader
+    {
+        internal static ServerFile ReadServerFileFromStream(Stream s)
+        {
+            JsonSerializer js = new JsonSerializer();
+            using (TextReader tr = new StreamReader(s))
+            {
+                return (ServerFile)js.Deserialize(tr,typeof(ServerFile));
+            }
+        }
+    }
+}
