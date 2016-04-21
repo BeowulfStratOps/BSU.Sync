91384116c14b7350c4fdf9869b986966251a02a4
James McCartney <jsm123@gmail.com>
2016-04-21 23:03:19 +0100

    Added the ability to check if ArmA is installed and get its path (similar to TS) closes T8

---
+++ ArmA.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSU.Sync
+{
+    public static class ArmA
+    {
+        /// <summary>
+        /// Determines if ArmA is installed
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsInstalled()
+        {
+            return ArmALocation() != string.Empty;
+        }
+        /// <summary>
+        /// Determines the location of ArmA 
+        /// </summary>
+        /// <returns></returns>
+        public static string ArmALocation()
+        {
+            RegistryKey localKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            localKey = localKey.OpenSubKey(@"SOFTWARE\Bohemia Interactive\ArmA 3");
+            if (localKey != null)
+            {
+                return localKey.GetValue("main").ToString();
+            }
+
+            RegistryKey localkey32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
+            localkey32 = localkey32.OpenSubKey(@"SOFTWARE\Bohemia Interactive\ArmA 3");
+            if (localkey32 != null)
+            {
+                return localkey32.GetValue("main").ToString();
+            }
+
+            return string.Empty;
+        }
+    }
+}
