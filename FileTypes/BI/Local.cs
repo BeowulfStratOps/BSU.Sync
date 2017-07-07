2ab2f9407e48d97e7bf78c4bd4e14e1693c21021
James McCartney <Jsm123@gmail.com>
2017-07-07 17:03:19 +0100

    Add Local class for (de)serializing BI's Local.json

---
+++ FileTypes/BI/Local.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BSU.Sync.FileTypes.BI
+{
+    class Local
+    {
+        [JsonProperty(propertyName: "autodetectionDirectories")]
+        public List<string> AutodetectionDirectories { get; set; }
+
+        [JsonProperty(propertyName: "dateCreated")]
+        public DateTime DateCreated { get; set; }
+
+        [JsonProperty(propertyName: "knownLocalMods")]
+        public List<string> KnownLocalMods { get; set; }
+
+        [JsonProperty(propertyName: "userDirectories")]
+        public List<string> UserDirectories { get; set; }
+    }
+}
